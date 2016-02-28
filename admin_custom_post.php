49ef1973fe7b5d5efa8dbb051073a748aa6b110d
Fuad Hassan <iFuad@Fuads-MacBook-Pro.local>
2016-02-28 21:09:15 +0600

    meta box added

--- admin_custom_post.php
+++ admin_custom_post.php
@@ -10,12 +10,39 @@ function register_text_form_admin_custom_post(){
 						),
 						'public' => true,
 						'has_archieve' => true,
-						'supports' => array( 'title','thumbnail','editor','excerpt','custom_field' ),
+						'supports' => array( 'title','thumbnail','editor','excerpt','custom_fields' ),						
 					)
 						
 	);
 }//end of function
 
+function meta_box_add_function(){
+		add_meta_box( 'text_from_admin_meta_box',
+					  'Admin meta box',
+					  'callback_add_meta_box',
+					  'text_from_admin',
+					  'normal',
+					  'high'
+					);	
+	
+}//end of function
+
+function callback_add_meta_box(){
+	
+?>
+	<div>
+    	<div style="float:left">
+        	Foot Note:
+        </div>
+        <div style="float:left">
+        	<input type="text" name="foot_note" />
+        </div>
+        <div style="clear:both"></div>
+    </div>
+<?php
+	
+}//end of function
 add_action( 'init', 'register_text_form_admin_custom_post');
+add_action ( 'admin_init', 'meta_box_add_function');
 
 ?>
